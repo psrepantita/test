b09dc3bf8b84a431479536ad25f5a4f43150a357
Pantita Soontaran <pantita.s+scbtechx@vasup.co.th>
2026-03-19 17:41:14 +0700

    robot cicd

--- tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title vasup', async ({ page }) => {
-  await page.goto('https://www.vasup.co.th/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/VASUP/);
-});
